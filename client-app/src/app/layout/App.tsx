d6054347da3aca7ada99c5f958187a7323798cca
Jordan Webber <webber_jordan_95@hotmail.com>
2021-05-11 11:00:36 -0400

    End of Section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,41 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./styles.css";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import { Route } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading App" />;
+  const location = useLocation();
 
   return (
     <>
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard/>
-      </Container>
+      <Route path="/" exact component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => {
+          return (
+          <>
+            <NavBar />
+            <Container style={{ marginTop: "7em" }}>
+              <Route path="/activities" exact component={ActivityDashboard} />
+              <Route path="/activities/:id" exact component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={["/createActivity", "/manage/:id"]}
+                exact
+                component={ActivityForm}
+              />
+            </Container>
+          </>
+          );
+        }}
+      />
     </>
   );
 }
