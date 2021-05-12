8a8057bc3bc080971ec4de8f1aa7e1176ce37d76
Jordan Webber <webber_jordan_95@hotmail.com>
2021-05-11 23:48:40 -0400

    End of Section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,31 +8,49 @@ import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import { useLocation } from "react-router-dom";
+import { Switch, useLocation } from "react-router-dom";
+import TestErrors from "../../features/errors/TestError";
+import { ToastContainer } from "react-toastify";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route path="/" exact component={HomePage} />
       <Route
-        path={'/(.+)'}
+        path={"/(.+)"}
         render={() => {
           return (
-          <>
-            <NavBar />
-            <Container style={{ marginTop: "7em" }}>
-              <Route path="/activities" exact component={ActivityDashboard} />
-              <Route path="/activities/:id" exact component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={["/createActivity", "/manage/:id"]}
-                exact
-                component={ActivityForm}
-              />
-            </Container>
-          </>
+            <>
+              <NavBar />
+              <Container style={{ marginTop: "7em" }}>
+                <Switch>
+                  <Route
+                    path="/activities"
+                    exact
+                    component={ActivityDashboard}
+                  />
+                  <Route
+                    path="/activities/:id"
+                    exact
+                    component={ActivityDetails}
+                  />
+                  <Route
+                    key={location.key}
+                    path={["/createActivity", "/manage/:id"]}
+                    exact
+                    component={ActivityForm}
+                  />
+                  <Route path="/errors" component={TestErrors} />
+                  <Route path="/server-error" component={ServerError} />
+                  <Route component={NotFound} />
+                </Switch>
+              </Container>
+            </>
           );
         }}
       />
