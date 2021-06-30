d5ed38f1b370361b3407c071ca3fea3942a77a97
Jordan Webber <webber_jordan_95@hotmail.com>
2021-06-30 17:13:26 -0400

    End of Section 12

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -28,11 +30,17 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers().AddFluentValidation(config => 
+      services.AddControllers(opt => 
+      {
+        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        opt.Filters.Add(new AuthorizeFilter(policy));
+      })
+        .AddFluentValidation(config => 
       {
         config.RegisterValidatorsFromAssemblyContaining<Create>();
       });
       services.AddApplicationServices(_config);
+      services.AddIdentityServices(_config);
       services.AddSwaggerGen();
     }
 
@@ -52,6 +60,7 @@ namespace API
 
       app.UseCors("CorsPolicy");
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
